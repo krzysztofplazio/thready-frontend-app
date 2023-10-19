0419d61eb5a0a6dbbc44a297e0dfce5047d5ca61
krzysztofplazio <krzysztofplazio@protonmail.com>
2023-10-19 22:14:34 +0200

    Added unfinished login.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 
 // 1. import `ChakraProvider` component
 // import Background from './components/main/Background';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import MainLayout from './components/layouts/main-layout/MainLayout';
+import Login from './components/layouts/login/Login';
 
 function App() {
+  const token = "";
+  if (token === ""){
+    <Navigate to='/sign-in' />
+  }
+
   return (
       <BrowserRouter>
-        <MainLayout />
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='*' element={<MainLayout />} />
+        </Routes>
       </BrowserRouter>
   );
 }
--- src/components/layouts/login/Login.tsx
+++ src/components/layouts/login/Login.tsx
@@ -1,8 +1,51 @@
+import { Box, FormControl, IconButton, Input, InputAdornment, InputLabel } from "@mui/material";
+import './Login.scss';
+import React from "react";
+import { AccountCircle, Visibility, VisibilityOff } from "@mui/icons-material";
+import KeyRoundedIcon from '@mui/icons-material/KeyRounded';
 
 export default function Login() {
   return (
-    <div className='App'>
-      <form>{/*code for signin form*/}</form>
-    </div>
+    <React.Fragment>
+      <div className="container">
+        <Box className='login-box'>
+          <FormControl variant="standard">
+            <InputLabel htmlFor="login">
+              Login
+            </InputLabel>
+            <Input
+                id="login"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <AccountCircle />
+                  </InputAdornment>
+                }
+              />
+          </FormControl>
+          <FormControl variant="standard">
+            <InputLabel htmlFor="password">
+              Password
+            </InputLabel>
+            <Input
+              id="outlined-adornment-password"
+              type={showPassword ? 'text' : 'password'}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+        </Box>
+      </div>
+    </React.Fragment>
   );
 };
